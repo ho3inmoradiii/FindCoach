2f64796a2e29d1b84f6cd578d3c6ca86df654970
Hossein Moradi <ho3inmoradi1998@gmail.com>
2021-09-06 01:03:57 -0700

    add base badge for style Specialties

--- src/components/coach/CoachItem.vue
+++ src/components/coach/CoachItem.vue
@@ -3,7 +3,7 @@
         <h3>{{ fullName }}</h3>
         <h4>${{ hourlyRate }}/hour</h4>
         <div>
-            <span v-for="area in areas" :key="area">{{ area }}</span>
+            <base-badge v-for="area in areas" :key="area" :type="area" :title="area">{{ area }}</base-badge>
         </div>
         <div class="actions">
             <the-button link mode="outline" :to="coachContactLink">ارتباط با مدرس</the-button>
